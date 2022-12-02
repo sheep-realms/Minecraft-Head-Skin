aade72c9022a99607e235f2d46063809e02d9666
Sheep-realms <284178977@qq.com>
2022-12-02 12:55:25 +0800

    Update

--- web/js/db.js
+++ web/js/db.js
@@ -16,6 +16,39 @@ let skinList = [
         "name": "Barrel Open 1",
         "url": "block/function/barrel_open_1.png"
     }, {
+        "name": "Beehive",
+        "url": "block/function/beehive.png"
+    }, {
+        "name": "Beehive Honey",
+        "url": "block/function/beehive_honey.png"
+    }, {
+        "name": "Chiseled Bookshelf",
+        "url": "block/function/chiseled_bookshelf.png"
+    }, {
+        "name": "Chiseled Bookshelf 1",
+        "url": "block/function/chiseled_bookshelf_1.png"
+    }, {
+        "name": "Chiseled Bookshelf 2",
+        "url": "block/function/chiseled_bookshelf_2.png"
+    }, {
+        "name": "Chiseled Bookshelf 3",
+        "url": "block/function/chiseled_bookshelf_3.png"
+    }, {
+        "name": "Chiseled Bookshelf 4",
+        "url": "block/function/chiseled_bookshelf_4.png"
+    }, {
+        "name": "Chiseled Bookshelf 5",
+        "url": "block/function/chiseled_bookshelf_5.png"
+    }, {
+        "name": "Chiseled Bookshelf 6",
+        "url": "block/function/chiseled_bookshelf_6.png"
+    }, {
+        "name": "Chiseled Bookshelf 7",
+        "url": "block/function/chiseled_bookshelf_7.png"
+    }, {
+        "name": "Lucky Block",
+        "url": "block/function/lucky_block.png"
+    }, {
         "type": "h2",
         "title": "自然方块 / Natural Block",
         "id": "t-natural-block"
